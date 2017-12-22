f187039629d25d3d48e1f3f3dccabbe56dff9140
Walker Hale IV <walker.hale.iv@gmail.com>
2017-12-21 22:11:32 -0600

    Bump version of mplx_qc to 1.1.0-rc1

--- bin/mplx_qc.py
+++ bin/mplx_qc.py
@@ -26,7 +26,7 @@ import openpyxl
 from dump_js_barcodes import Merge
 from dump_js_barcodes import SequencingEvent
 
-__version__ = '1.0.0-rc3'
+__version__ = '1.1.0-a0'
 
 logger = logging.getLogger(__name__)
 
