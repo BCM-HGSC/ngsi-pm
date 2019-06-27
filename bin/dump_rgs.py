e25cf26f64c428bdf11dca6eb13a9898c5bb81e2
Marcela Torres <itsmarcy@gmail.com>
2019-06-27 13:18:21 -0500

    Remove uncessary commented code

--- bin/dump_rgs.py
+++ bin/dump_rgs.py
@@ -9,27 +9,19 @@ def main():
 
 
 def run():
-    # read file, get rg_bc, rg_sm
-    # filename = '20_rg.txt'
-    # with open(filename) as fin:
     for line in sys.stdin:
         linesplit = line.rstrip().split('\t')
         if linesplit[0] != '@RG':
             continue
-        # print (linesplit)
         rg_dict = {}
         for item in linesplit[1:]:
             k, v = item.split(':', 1)
             assert k not in rg_dict, (k, rg_dict)
             assert ':' not in k
             rg_dict[k] = v
-        # print(rg_dict.items())
-        # update rg_bc for new HgV 17.5
         pat = re.compile(r'PU:(?:[\w-]+_)?([\w-]+)')
         rg_bc = pat.search(line).group(1)
-        # rg_bc = rg_dict['PU']
         rg_sm = rg_dict['SM']
-        #  print('PU: %s SM: %s' % (rg_bc, rg_sm))
         print(rg_bc, rg_sm, sep='\t')
 
 
